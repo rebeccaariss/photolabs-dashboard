1bf3cab68b34d195c6fb712fec4f5c7038535455
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-10-09 14:41:47 -0400

    Retrieve data from PhotoLabs API

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,41 +4,66 @@ import Panel from "./Panel";
 
 import classnames from "classnames";
 
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "helpers/selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    photos: [],
+    topics: []
   };
 
   componentDidMount() {
+    // Persist state across sessions using local storage in browser:
     const focused = JSON.parse(localStorage.getItem("focused"));
 
     if (focused) {
       this.setState({ focused });
     }
+
+    // PhotoLabs API logic (triggered after component [Dashboard] render):
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
+    
+    Promise.all(urlsPromise)
+    .then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics
+      });
+    });
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -63,8 +88,8 @@ class Dashboard extends Component {
       <Panel 
         key={panel.id} 
         label={panel.label} 
-        value={panel.value}
-        onSelect={event => this.selectPanel(panel.id)}
+        value={panel.getValue(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
       />
     ));
 
